6a8b566dc3c2a5ff02c119c6825a5e10c43af0fe
JunqiHu23 <junqi.hu.21@ucl.ac.uk>
2021-10-29 12:12:30 +0100

    add world2

--- homework4.R
+++ homework4.R
@@ -1,10 +1,14 @@
 library(sf)
 library(here)
 library(tidyverse)
+
 HDI <- read.csv(here('Data','Gender Inequality Index (GII).csv'),
                 header = TRUE,
                 sep = ",",
                 skip = 5,
                 na="..",
-                encoding = "latin1"
-                )
+                encoding = "latin1")
+
+World <- st_read(here('Data',
+                      'World_Countries_(Generalized)',
+                      'World_Countries__Generalized_.shp'))
